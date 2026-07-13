a87d57b2bb52903a9f0dbeca336655fffd2765b5
rdh <me@rdh540.dev>
2026-07-13 19:36:59 +0530

    use compatible particle render condition

--- src/main/java/org/taumc/celeritas/mixin/core/ParticleManagerMixin.java
+++ src/main/java/org/taumc/celeritas/mixin/core/ParticleManagerMixin.java
@@ -4,6 +4,8 @@ import net.minecraft.client.ParticleManager;
 import net.minecraft.client.entity.particle.Particle;
 import net.minecraft.client.render.vertex.BufferBuilder;
 import net.minecraft.entity.Entity;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -11,15 +13,13 @@ import org.taumc.celeritas.impl.extensions.ParticleExtension;
 
 @Mixin(ParticleManager.class)
 public abstract class ParticleManagerMixin {
-    @Redirect(
+    @WrapWithCondition(
             method = {"render", "renderLit"},
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/particle/Particle;render(Lnet/minecraft/client/render/vertex/BufferBuilder;Lnet/minecraft/entity/Entity;FFFFFF)V"),
             require = 2
     )
-    private void celeritas$renderVisibleParticle(Particle particle, BufferBuilder buffer, Entity camera,
+    private boolean celeritas$renderVisibleParticle(Particle particle, BufferBuilder buffer, Entity camera,
             float tickDelta, float rotationX, float rotationZ, float rotationYZ, float rotationXY, float rotationXZ) {
-        if (((ParticleExtension)particle).celeritas$isVisible()) {
-            particle.render(buffer, camera, tickDelta, rotationX, rotationZ, rotationYZ, rotationXY, rotationXZ);
-        }
+        return ((ParticleExtension) particle).celeritas$isVisible();
     }
 }
