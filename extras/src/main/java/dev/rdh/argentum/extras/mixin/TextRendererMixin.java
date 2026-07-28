351750137d8ce9023c688ac45b43b8b5bce9febf
rdh <me@rdh540.dev>
2026-07-28 12:07:35 -0400

    use a more compatible injector to disable text shadows

--- extras/src/main/java/dev/rdh/argentum/extras/mixin/TextRendererMixin.java
+++ extras/src/main/java/dev/rdh/argentum/extras/mixin/TextRendererMixin.java
@@ -1,26 +1,22 @@
 package dev.rdh.argentum.extras.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
-import dev.rdh.argentum.extras.ArgentumExtras;
-import net.minecraft.client.render.TextRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// Credits to moehreag for this!
+import dev.rdh.argentum.extras.ArgentumExtras;
+
+import net.minecraft.client.render.TextRenderer;
+
 @Mixin(TextRenderer.class)
 public class TextRendererMixin {
-
-	@Inject(
-		method = "draw(Ljava/lang/String;FFIZ)I",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/TextRenderer;reset()V")
-	)
-	private void argentumExtras$disableTextShadows(String text, float x, float y, int color, boolean shadow, CallbackInfoReturnable<Integer> cir, @Local(argsOnly = true) LocalBooleanRef renderShadow
-	) {
-    if (ArgentumExtras.CONFIG.disableTextShadows) {
-		  renderShadow.set(false);
-    }
+	@Definition(id = "shadow", local = @Local(type = boolean.class, argsOnly = true))
+	@Expression("shadow")
+	@ModifyExpressionValue(method = "draw(Ljava/lang/String;FFIZ)I", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean argentumExtras$disableTextShadows(boolean original) {
+		return original && !ArgentumExtras.CONFIG.disableTextShadows;
 	}
 }
