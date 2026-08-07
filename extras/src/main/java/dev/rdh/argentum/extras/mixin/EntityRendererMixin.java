a0164a324720cf4db2d6717c7a84063c69b74099
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-08-07 13:58:57 +0100

    Update EntityRendererMixin.java

--- extras/src/main/java/dev/rdh/argentum/extras/mixin/EntityRendererMixin.java
+++ extras/src/main/java/dev/rdh/argentum/extras/mixin/EntityRendererMixin.java
@@ -18,9 +18,9 @@ public class EntityRendererMixin {
 
     @SuppressWarnings("WrapWithConditionTargetsNonVoid")
     @WrapWithCondition(method = "renderNameTag(Lnet/minecraft/entity/Entity;Ljava/lang/String;DDDI)V", at = {
-            @At(value = "INVOKE", target = "Lnet/minecraft/client/render/platform/GlStateManager;enableDepthTest()V", ordinal = 0),
+            @At(value = "INVOKE", target = "Lnet/minecraft/client/render/platform/GlStateManager;enableDepthTest()V", ordinal = 1),
             @At(value = "INVOKE", target = "Lnet/minecraft/client/render/platform/GlStateManager;depthMask(Z)V", ordinal = 1),
-            @At(value = "INVOKE", target = "Lnet/minecraft/client/render/TextRenderer;draw(Ljava/lang/String;III)I", ordinal = 1)
+            @At(value = "INVOKE", target = "Lnet/minecraft/client/render/TextRenderer;draw(Ljava/lang/String;III)I", ordinal = 2)
     })
     private boolean argentumExtras$disableSecondNameTagLayer() {
         return ArgentumExtras.CONFIG.secondNameTagLayer;
