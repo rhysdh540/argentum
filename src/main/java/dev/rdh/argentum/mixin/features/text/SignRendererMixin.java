d978dc332553de2a98fb32ea2fb835bfdae6f862
rdh <me@rdh540.dev>
2026-09-03 22:34:17 -0400

    sign batching!

--- src/main/java/dev/rdh/argentum/mixin/features/text/SignRendererMixin.java
+++ src/main/java/dev/rdh/argentum/mixin/features/text/SignRendererMixin.java
@@ -9,13 +9,41 @@ import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.render.TextRenderer;
 import net.minecraft.client.render.block.entity.SignRenderer;
 import net.minecraft.text.Text;
+
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
 @Mixin(SignRenderer.class)
 public class SignRendererMixin {
+    @Inject(
+            method = "render(Lnet/minecraft/block/entity/SignBlockEntity;DDDFI)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/platform/GlStateManager;depthMask(Z)V",
+                    ordinal = 0, shift = At.Shift.AFTER)
+    )
+    private void argentum$beginSignText(SignBlockEntity sign, double x, double y, double z, float tickDelta,
+                                        int breakProgress, CallbackInfo ci, @Local TextRenderer textRenderer,
+                                        @Share("textRenderer") LocalRef<TextRenderer> bruh) {
+        textRenderer.argentum$beginBatch(() -> {});
+        bruh.set(textRenderer); // mixin actually pmo
+    }
+
+    @Inject(
+            method = "render(Lnet/minecraft/block/entity/SignBlockEntity;DDDFI)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/platform/GlStateManager;depthMask(Z)V",
+                    ordinal = 1)
+    )
+    private void argentum$endSignText(SignBlockEntity sign, double x, double y, double z, float tickDelta,
+                                      int breakProgress, CallbackInfo ci,
+                                      @Share("textRenderer") LocalRef<TextRenderer> bruh) {
+        bruh.get().argentum$endBatch();
+    }
+
     @WrapOperation(
             method = "render(Lnet/minecraft/block/entity/SignBlockEntity;DDDFI)V",
             at = @At(value = "INVOKE",
